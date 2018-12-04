7ff091ab0f1166ef57d97afc8181e5d1e4409b27
aaroon9 <adrian.valera.garcia@gmail.com>
2018-12-04 17:52:33 +0100

    Tasca 1 acabada
    Faltara tenir en compte les puntuacion y la pujada de nivel cada 10 peces.

---
+++ tetris.js
@@ -0,0 +1,52 @@
+var estadoActual = function(){
+    var actual = new Array(25);
+        actual[00] = [0,0,0,0,0,0,0,0,0,0];
+        actual[01] = [0,0,0,0,0,0,0,0,0,0];
+        actual[02] = [0,0,0,0,0,0,0,0,0,0];
+        actual[03] = [0,0,0,0,0,0,0,0,0,0];
+        actual[04] = [0,0,0,0,0,0,0,0,0,0];
+        actual[05] = [0,0,0,0,0,0,0,0,0,0];
+        actual[06] = [0,0,0,0,0,0,0,0,0,0];
+        actual[07] = [0,0,0,0,0,0,0,0,0,0];
+        actual[08] = [0,0,0,0,0,0,0,0,0,0];
+        actual[09] = [0,0,0,0,0,0,0,0,0,0];
+        actual[10] = [0,0,0,0,0,0,0,0,0,0];
+        actual[11] = [0,0,0,0,0,0,0,0,0,0];
+        actual[12] = [0,0,0,0,0,0,0,0,0,0];
+        actual[13] = [0,0,0,0,0,0,0,0,0,0];
+        actual[14] = [0,0,0,0,0,0,0,0,0,0];
+        actual[15] = [0,0,0,0,0,0,0,0,0,0];
+        actual[16] = [0,0,0,0,0,0,0,0,0,0];
+        actual[17] = [0,0,0,0,0,0,0,0,0,0];
+        actual[18] = [0,0,0,0,0,0,0,0,0,0];
+        actual[19] = [0,0,0,0,0,0,0,0,0,0];
+        actual[20] = [0,0,0,0,0,0,0,0,0,0];
+        actual[21] = [0,0,0,0,0,0,0,0,0,0];
+        actual[22] = [0,0,0,0,0,0,0,0,0,0];
+        actual[23] = [0,0,0,0,0,0,0,0,0,0];
+        actual[24] = [0,0,0,0,0,0,0,0,0,0];
+    statusTauler: 1;
+    puntuacioJugador: 0;
+    puntuacioMaxima: 0;
+    piezaActual: "new Object";
+    piezaSig: "new Object";
+    contadorPieza: new Array(7);
+    interval: 1000;
+    //parametro interval modificado hace que el juego vaya mas rapido*.
+    //var corre = setInterval(jugar, this.interval);
+    
+    iniciarJuego: function(){
+        
+    }
+    calcSigPieza: function(){
+        
+    }
+    teclas: function(){
+        
+    }
+    mAuto: function(interval){
+        
+    }
+    
+    
+}
